cecda0be438bef64837c871edd7f40ffcc5a457f
bayronaxpuac95 <48611884+romeoaxpuac123@users.noreply.github.com>
2021-01-09 20:47:50 -0600

    Update procedimientos.sql

--- BBDD/procedimientos.sql
+++ BBDD/procedimientos.sql
@@ -36,3 +36,35 @@ AS
 		WHERE Codigo_Compra = @Codigo_Compra; 
 		
 GO
+CREATE PROCEDURE CrearFactura   
+    @Total int,   
+    @Fecha date,
+	@Codigo_Cliente int
+AS   
+	INSERT INTO Factura (Total,Fecha,Codigo_Cliente) VALUES (@Total,@Fecha,@Codigo_Cliente);
+	SELECT TOP 1 Codigo_Factura FROM Factura ORDER BY Codigo_Factura DESC
+GO
+CREATE PROCEDURE RegistrarVenta
+	@Codigo_Factura int,
+    @Codigo_Producto int,   
+    @Codigo_Sucursal int,
+	@Cantidad int,
+	@Ultima_Cantidad int,
+	@Fecha_Adquisicion date 
+AS   
+	INSERT INTO Venta(Codigo_Factura,Codigo_Producto,Codigo_Sucursal,Cantidad,Ultima_Cantidad,Fecha_Adquisicion) 
+		VALUES (@Codigo_Factura,@Codigo_Producto,@Codigo_Sucursal,@Cantidad,@Ultima_Cantidad,@Fecha_Adquisicion) ;
+	UPDATE Producto_Sucursal SET Cantidad=Cantidad  - @Cantidad  
+		WHERE Codigo_Producto=@Codigo_Producto AND Codigo_Sucursal = @Codigo_Sucursal; 
+		
+GO
+CREATE PROCEDURE TotalFactura
+	@Codigo_Factura int,
+	@Codigo_Producto int,
+	@Cantidad int
+AS   
+
+	UPDATE Factura SET Total=Total + ((SELECT Precio_Venta FROM Producto WHERE Codigo_Producto = @Codigo_Producto) * @Cantidad)
+		WHERE Codigo_Factura = @Codigo_Factura; 
+		
+GO
