eb666ca60955e2f0f851110d6d8bc2f737082067
bayronaxpuac95 <48611884+romeoaxpuac123@users.noreply.github.com>
2021-01-09 22:32:23 -0600

    Procedimientos Inventario

--- BBDD/procedimientos.sql
+++ BBDD/procedimientos.sql
@@ -68,3 +68,55 @@ AS
 		WHERE Codigo_Factura = @Codigo_Factura; 
 		
 GO
+CREATE PROCEDURE CatalogoSucursal
+	@Codigo_Sucursal int
+AS   
+
+	SELECT T.Nombre, T.Descripcion,T.Categoria, T.Codigo_Producto, T.Precio_Venta, PS.Cantidad AS Stock,
+	PS.Fecha_Adquisicion AS Abastecimiento, PS.Ultima_Cantidad AS Recarga FROM Producto T, Producto_Sucursal PS
+		WHERE (T.Codigo_Producto = PS.Codigo_Producto) AND (PS.Codigo_Sucursal = @Codigo_Sucursal); 
+		
+GO
+CREATE PROCEDURE ProductosPorCategoria
+	@Codigo_Sucursal int,
+	@Categoria varchar(50)
+AS   
+
+	SELECT T.Nombre, T.Descripcion,T.Categoria, T.Codigo_Producto, T.Precio_Venta, PS.Cantidad AS Stock,
+	PS.Fecha_Adquisicion AS Abastecimiento, PS.Ultima_Cantidad AS Recarga FROM Producto T, Producto_Sucursal PS
+		WHERE (T.Codigo_Producto = PS.Codigo_Producto) AND (PS.Codigo_Sucursal = @Codigo_Sucursal)AND (T.Categoria = @Categoria);
+		
+GO
+CREATE PROCEDURE ProductosPorNombre
+	@Codigo_Sucursal int,
+	@Nombre varchar(50)
+AS   
+
+	SELECT T.Nombre, T.Descripcion,T.Categoria, T.Codigo_Producto, T.Precio_Venta, PS.Cantidad AS Stock,
+	PS.Fecha_Adquisicion AS Abastecimiento, PS.Ultima_Cantidad AS Recarga FROM Producto T, Producto_Sucursal PS
+		WHERE (T.Codigo_Producto = PS.Codigo_Producto) AND (PS.Codigo_Sucursal = @Codigo_Sucursal)AND (T.Nombre = @Nombre);
+		
+GO
+CREATE PROCEDURE ProductosPorCodigo
+	@Codigo_Sucursal int,
+	@Codigo int
+AS   
+
+	SELECT T.Nombre, T.Descripcion,T.Categoria, T.Codigo_Producto, T.Precio_Venta, PS.Cantidad AS Stock,
+	PS.Fecha_Adquisicion AS Abastecimiento, PS.Ultima_Cantidad AS Recarga FROM Producto T, Producto_Sucursal PS
+		WHERE (T.Codigo_Producto = PS.Codigo_Producto) AND (PS.Codigo_Sucursal = @Codigo_Sucursal)AND (T.Codigo_Producto = @Codigo);
+		
+GO
+CREATE PROCEDURE TotalJuguetes
+	@Codigo_Sucursal int
+AS   
+
+	SELECT SUM(Cantidad) AS Total_Juguetes FROM Producto_Sucursal WHERE Codigo_Sucursal = @Codigo_Sucursal ;
+GO
+CREATE PROCEDURE Promedio
+	@Codigo_Sucursal int
+AS   
+
+	SELECT AVG(P.Precio_Venta) AS Promedio FROM Producto P, Producto_Sucursal PS 
+	WHERE P.Codigo_Producto = PS.Codigo_Producto AND (PS.Codigo_Sucursal = @Codigo_Sucursal);
+GO
